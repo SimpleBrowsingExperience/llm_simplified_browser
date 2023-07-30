ee1892ee55f26a396926ae5723cc040bde2f788f
joa-stdn <jstudnia@stanford.edu>
2023-07-30 08:51:54 -0700

    Update main.py with comments

--- main.py
+++ main.py
@@ -1,71 +1,112 @@
+"""
+Anthropic Hackathon
+This is the main script that allows browsing with interactions
+on SimpleBrowser
+"""
+
 from actions_from_website import *
 
+# Defining target websites
 actual_site = "https://www.bing.com/search?q=pancakes"
 selection = ""
 
+
 def generate_page(name, summary, elements, forms, clicks):
+    """
+    Generates HTML code for the simplified page
+    and writes output in a file
+    """
+    # Headers
+    html = f"<html>"
+    html += f"<head><title>{name}</title>"
+    html += """
+    <link rel="stylesheet" type="text/css" href="style.css">"""
 
-  html = f"<html>"
-  html += f"<head><title>{name}</title>"
-  html += """
-  <link rel="stylesheet" type="text/css" href="style.css">"""
+    # Adding name and summary
+    html += f"</head><body><h1>{name}</h1>"
+    html += f"<p>{summary}</p>"
 
-  html += f"</head><body><h1>{name}</h1>"
-  html += f"<p>{summary}</p>"
+    # Adding elements if they exist
+    for url, text in elements:
+        html += f"<div class='element'><a href='{url}'>{text}</a></div>"
+    
+    # Adding actions
+    html += "<div>"
+    for form in forms:
+        html += f"<div class='action'>{form}</div>"
 
-  for url, text in elements:
-    html += f"<div class='element'><a href='{url}'>{text}</a></div>"
-  
-  html += "<div>"
-  for form in forms:
-    html += f"<div class='action'>{form}</div>"
+    for url, text in clicks:
+        html += f"<br><div class='action'>>> <a href='{url}'>{text}</a></div>"
 
-  for url, text in clicks:
-    html += f"<br><div class='action'>>> <a href='{url}'>{text}</a></div>"
+    html += "</div></body></html>"
 
-  html += "</div></body></html>"
+    # Writing output in an HTML file
+    fichier = open(f"pages/{name}.html", "w")
+    fichier.write(html)
+    fichier.close()
 
-  fichier = open(f"pages/{name}.html", "w")
-  fichier.write(html)
-  fichier.close()
 
+# Main code
 while True:
+    # Interactions with console mode
     print(f"Fetching URL: {actual_site}")
-    html = get_simplified_html(actual_site)
-    name, summary, elements = get_elements(html)
 
+    # Getting simplified HTML code
+    html = get_simplified_html(actual_site)
+
+    # Retrieving name, summary, elements
+    name, summary, elements = get_elements(html)
     print(f"Website: {name}")
     print(f"Summary: {summary}")
 
+    # Showing elements and associated keys
     i = 1
     for element in elements:
         print(f"Option {i} : {element[1]}")
         i += 1
 
+    # Retrieving actions
     forms, clicks = get_actions(html)
 
+    # Showing actions and associated keys
     for form in forms:
         print(f"Option {i}: {form}")
         i += 1
+    
     for click in clicks:
         print(f"Option {i}: {click[1]}")
         i += 1
+    
+    # Quit option
     print("q: quit")
+
+    # Generates HTML code of the simplified page
     generate_page(name, summary, elements, forms, clicks)
+
+    # Asking user for selection
     selection = input("Select an option: ")
+
     if selection == "q":
+        # Quitting
         break
+    
     elif selection.isdigit():
         selection = int(selection) - 1 
+    
     else:
         print("Invalid selection")
         continue
+    
     if selection < len(elements):
+        # Clicking on an element
         actual_site = elements[selection][0]
+    
     elif selection < len(forms):
+        # Forms are not yet implemented
         print("Not implemented yet")
+    
     elif selection < len(clicks):
         actual_site = clicks[selection][0]
+    
     else:
         print("Invalid selection")
-
