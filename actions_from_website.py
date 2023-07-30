65ccd80fb465728f73cf4907d483225456e99bd3
joa-stdn <jstudnia@stanford.edu>
2023-07-30 08:52:39 -0700

    Update actions_from_website.py comments

--- actions_from_website.py
+++ actions_from_website.py
@@ -1,4 +1,7 @@
-# Anthropic hackathon
+"""
+Anthropic hackathon
+This file contains all auxiliary functions our simple browser needs
+"""
 
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 from secrets_api import *
@@ -7,22 +10,44 @@ import bs4
 from urllib.request import Request, urlopen
 import time
 
+
 DEBUG = False
 
+
 def get_html(url):
+    """
+    Given a url as input, returns the raw HTML code
+    """
+
+    # Pretending to be a real browser eg Mozilla
+    # To avoid being seen as a bot
     req = Request(
         url=url, 
         headers={'User-Agent': 'Mozilla/5.0'}
     )
+
+    # Debug mode
     if DEBUG:
         print(f"Requesting {url}")
+
+    # Sleeping for 2 sec
     time.sleep(2)
+
+    # Opening and reading HTML code
     mybytes = urlopen(req).read()
     mystr = mybytes.decode("utf8")
     return mystr
 
+
 def fast_simplify_html(source_html):
-   # Parse the HTML content using BeautifulSoup
+    """
+    This function preprocesses HTML code to avoid sending Claude
+    too many tokens as context
+    Recall that most of the tokens in an HTML code are actually useless
+    to understand the meaning of a page
+    """
+
+    # Parse the HTML content using BeautifulSoup
     simplified_soup = bs4.BeautifulSoup(source_html, 'html.parser')
     
     # Remove attributes from all tags except href
@@ -34,6 +59,7 @@ def fast_simplify_html(source_html):
     for script_tag in simplified_soup.find_all('script'):
         script_tag.extract()
     
+    # Remove all style tags and their content
     for style_tag in simplified_soup.find_all('style'):
         style_tag.extract()
 
@@ -49,52 +75,88 @@ def fast_simplify_html(source_html):
         comment.extract()
     
     # Return the simplified HTML
-    #print(str(simplified_soup))
     return str(simplified_soup)
 
 
 def get_simplified_html(url):
+    """
+    This function is simply composing get_html with fast_simplify_html
+    """
     return fast_simplify_html(get_html(url))
 
-###
 
 def parse_actions(text):
-  form_actions = re.findall(r'<action-form>(.*?)</action-form>', text)
-  click_actions = re.findall(r'<action-click url="(.*?)">(.*?)</action-click>', text)
-  return form_actions, click_actions
+    """
+    This is the parser for available actions, in a XML like format
+    Distinguishing between form actions and click actions
+    """
+    form_actions = re.findall(r'<action-form>(.*?)</action-form>', text)
+    click_actions = re.findall(r'<action-click url="(.*?)">(.*?)</action-click>', text)
+    return form_actions, click_actions
 
 def parse_elements(text):
-  name = re.findall(r'<name>(.*?)</name>', text)[0]
-  elements = re.findall(r'<element url="(.*?)">(.*?)</element>', text)
-  summary = re.findall(r'<summary>(.*?)</summary>', text)[0]
-  return name, summary, elements
+    """
+    This is the parser for the name of the website,
+    the summary of the page and available elements, in a XML like format
+    """
+    name = re.findall(r'<name>(.*?)</name>', text)[0]
+    elements = re.findall(r'<element url="(.*?)">(.*?)</element>', text)
+    summary = re.findall(r'<summary>(.*?)</summary>', text)[0]
+    return name, summary, elements
 
 def get_actions(html):
+    """
+    This function submits the Claude query with the actions prompt
+    to retrieve available actions in XML format
+    """
+    # Retrieving the actions prompt
     fichier = open("actions_prompt.txt", "r")
     FILE_PROMPT = fichier.read()
     fichier.close()
+
+    # Debug mode
     if DEBUG:
         print("API request of actions")
+
+    # API query
     completion = anthropic.completions.create(
         model="claude-2",
         max_tokens_to_sample=1000,
         prompt=f"{HUMAN_PROMPT} {FILE_PROMPT} <file>{html}</file> {AI_PROMPT}",
     )
+
+    # Debug mode
     if DEBUG:
             print(completion.completion)
+
+    # Parsing actions
     return parse_actions(completion.completion)
 
+
 def get_elements(html):
+    """
+    This function submits the Claude query with the elements prompt
+    to retrieve available elments in XML format
+    """
+    # Retrieving the actions prompt
     fichier = open("elements_prompt.txt", "r")
     FILE_PROMPT = fichier.read()
     fichier.close()
+    
+    # Debug mode
     if DEBUG:
         print("API request for elements")
+    
+    # API query
     completion = anthropic.completions.create(
         model="claude-2",
         max_tokens_to_sample=1000,
         prompt=f"{HUMAN_PROMPT} {FILE_PROMPT} <file>{html}</file> {AI_PROMPT}",
     )
+
+    # Debug mode
     if DEBUG:
         print(completion.completion)
+
+    # Parsing elements
     return parse_elements(completion.completion)
